cd3ec4834cfa3d2db7934df2368eb887e768ae9c
mschrot <mschrot@protonmail.com>
2025-10-31 15:04:49 +0100

    Add files via upload

---
+++ folge_44_python_game_aufgabe.py
@@ -0,0 +1,8 @@
+# 📝 Aufgabenstellung
+
+# Programmiere in Python ein kleines Glücksspiel mit Emojis.
+# Das Programm soll zufällig drei Emojis aus einer Liste auswählen und anzeigen.
+# ["🍒", "🍉", "⭐", "🍋", "💎"]
+# Sind alle drei Emojis gleich, soll eine Gewinnmeldung ausgegeben werden: 🎉 Gewonnen!
+# Sind sie unterschiedlich, erscheint: 🙁 Verloren!
+# Nutze dafür das Modul random.
